b8b7cc32500c71abe6df7cb9d1da6a5a52dbe470
Rhyine <rhyinestewart@gmail.com>
2021-12-02 12:06:16 -0500

    Generate cars controller

---
+++ app/controllers/v1/cars_controller.rb
@@ -0,0 +1,13 @@
+class V1::CarsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
---
+++ test/controllers/v1/cars_controller_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+class V1::CarsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get v1_cars_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get v1_cars_show_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get v1_cars_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get v1_cars_create_url
+    assert_response :success
+  end
+end
