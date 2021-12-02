16edeeb36e04a6630a66b3b671d80cd050128d4e
Kevin <kevinsevinche@gmail.com>
2021-12-02 11:30:26 -0500

    Add validations to models

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,4 +1,5 @@
 class Rental < ApplicationRecord
   belongs_to :user
   belongs_to :car
+  validates :start_date, :end_date, :user_id, :car_id, :city, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
-  has_many :rentals
+  has_many :rentals, dependent: :destroy
+  validates :username, uniqueness: true, length: { minimum: 6 }
 end
