41faee8f2efe0ae403f4aa6b2d5306b20720d1b8
Kevin <kevinsevinche@gmail.com>
2021-12-02 17:15:52 -0500

    Update car, and user models

--- app/models/car.rb
+++ app/models/car.rb
@@ -3,4 +3,5 @@ class Car < ApplicationRecord
   validates :brand, :model, :seats_number, :transmision, :mileage, :image, :price_for_day, :bags_number, presence: true
   validates :seats_number, :bags_number, numericality: { only_integer: true, greater_than: 0 }
   validates :price_for_day, numericality: { only_float: true, greater_than: 0 }
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_many :rentals, dependent: :destroy
+  has_many :cars, dependent: :destroy
   validates :username, uniqueness: true, length: { minimum: 6 }
 end
