e0d58058841228bd84227ea5f7a82340bc67706d
Rhyine <rhyinestewart@gmail.com>
2021-12-02 12:56:56 -0500

    Create methods for controller

--- app/controllers/v1/cars_controller.rb
+++ app/controllers/v1/cars_controller.rb
@@ -1,13 +1,52 @@
 class V1::CarsController < ApplicationController
+  before_action :all_cars, only: %i[index]
+  before_action :set_car, only: %i[show]
+  # before_action :car_params
+
   def index
+    render json: @cars
   end
 
   def show
+    render json: @car
   end
 
-  def new
+  def destroy
   end
 
   def create
+    p car_params
+    # user = current_user
+    car = Car.new(car_params)
+    # car.owner = user
+    response = if car.save
+                 { car: car }
+               else
+                 { message: "Car didn't save" }
+               end
+    json_response(response)
+  end
+
+  private
+
+  def all_cars
+    @cars = Car.all
+  end
+
+  def set_car
+    @car = Car.find(params[:id])
+  end
+
+  def car_params
+    params.permit(
+      :brand,
+      :model,
+      :seats_number,
+      :transmision,
+      :mileage,
+      :image,
+      :price_for_day,
+      :bags_number
+    )
   end
 end
