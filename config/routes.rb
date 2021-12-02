bbe6286e41f0b57adeb2cb87536eeaeaf80421c5
Rhyine <rhyinestewart@gmail.com>
2021-12-02 12:05:53 -0500

    Configure routes for controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :v1 do
+    get 'cars/index'
+    get 'cars/show'
+    get 'cars/new'
+    get 'cars/create'
+  end
+  namespace :v1, defaults: { format: 'json' } do
+    resources :user
+    resources :cars
+    resources :rentals
+  end
 end
