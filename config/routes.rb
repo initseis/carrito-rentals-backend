30f667bda6ecffc244db93b29252baa983a402c0
Rhyine <rhyinestewart@gmail.com>
2021-12-02 12:07:19 -0500

    Configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  namespace :v1 do
-    get 'cars/index'
-    get 'cars/show'
-    get 'cars/new'
-    get 'cars/create'
-  end
   namespace :v1, defaults: { format: 'json' } do
     resources :user
     resources :cars
